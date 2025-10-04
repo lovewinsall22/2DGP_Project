cea40a519d0c4484fcc58f3ee39e7f215a7b884a
lovewinsall22 <taehyun0222@naver.com>
2025-10-04 14:37:56 +0900

    데미지 출력 위로 올라가다 사라지게 구현

--- 2DProject.py
+++ 2DProject.py
@@ -3,12 +3,13 @@ import math
 
 class DmgText:
     def __init__(self,x,y,damage):
-        self.font = load_font('K_Damage.ttf', 25)
+        self.font = load_font('K_Damage.ttf', 20)
         self.x, self.y = x,y
         self.damage = damage
         self.timer = 60
 
     def update(self):
+        self.y += 5
         self.timer -= 1
         return self.timer > 0
 
