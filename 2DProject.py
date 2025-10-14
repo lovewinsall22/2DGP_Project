5dcaff32b148c5b4b1f0a877de25aa2543347454
lovewinsall22 <taehyun0222@naver.com>
2025-10-14 18:58:57 +0900

    PlayerUI 클래스 모듈화

--- 2DProject.py
+++ 2DProject.py
@@ -3,6 +3,7 @@ import math
 
 from dmg_font import DmgText
 from npc import NPC
+from player_ui import PlayerUI
 from store import Store
 
 
@@ -168,27 +169,6 @@ class Player:
                     self.already_hit.add(m)
                     damage_texts.append(DmgText(m.x, m.y + 30, self.damage))
 
-class PlayerUI:
-    def __init__(self,player):
-        self.player = player
-        self.font = load_font('DNFBitBitTTF.ttf', 20)
-
-    def draw(self):
-        x1, y1 = WIDTH - 500, 20
-        bar_width = 480
-        bar_height = 30
-
-        hp_ratio = self.player.hp / self.player.max_hp
-        draw_rectangle(x1, y1, x1 + bar_width, y1 + bar_height)  # 테두리
-
-        fill_w = int(bar_width * hp_ratio)
-        for i in range(0, fill_w, 5):  # 5픽셀 단위로 채움
-            draw_rectangle(x1 + i, y1, x1 + i + 5, y1 + bar_height)
-
-        self.font.draw(WIDTH - 500, y1 + 50, f'HP: {int(self.player.hp)}/{self.player.max_hp}', (255, 255, 255))
-
-    def update(self):
-        pass
 
 def init_world():
     global running; running = True
---
+++ player_ui.py
@@ -0,0 +1,24 @@
+from pico2d import load_font, draw_rectangle
+WIDTH, HEIGHT = 1280, 720
+
+class PlayerUI:
+    def __init__(self,player):
+        self.player = player
+        self.font = load_font('DNFBitBitTTF.ttf', 20)
+
+    def draw(self):
+        x1, y1 = WIDTH - 500, 20
+        bar_width = 480
+        bar_height = 30
+
+        hp_ratio = self.player.hp / self.player.max_hp
+        draw_rectangle(x1, y1, x1 + bar_width, y1 + bar_height)  # 테두리
+
+        fill_w = int(bar_width * hp_ratio)
+        for i in range(0, fill_w, 5):  # 5픽셀 단위로 채움
+            draw_rectangle(x1 + i, y1, x1 + i + 5, y1 + bar_height)
+
+        self.font.draw(WIDTH - 500, y1 + 50, f'HP: {int(self.player.hp)}/{self.player.max_hp}', (255, 255, 255))
+
+    def update(self):
+        pass
