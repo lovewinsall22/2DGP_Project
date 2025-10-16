7a80dbd30aeca57aa868bd5068117df6356dcb61
lovewinsall22 <taehyun0222@naver.com>
2025-10-16 12:52:03 +0900

    portal 클래스 모듈화 + 오브젝트 삭제 코드 수정

---
+++ portal.py
@@ -0,0 +1,27 @@
+from pico2d import load_image, draw_rectangle
+from town import Town
+from npc import NPC
+from hit_objects.dummy import Dummy
+
+WIDTH, HEIGHT = 1280, 720
+
+class Portal:
+    def __init__(self,number,x,y):
+        self.image = load_image('resource/portal.png')
+        self.number = number
+        self.x , self.y = x, y
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x,self.y,64,64)
+        draw_rectangle(self.x - 32, self.y - 32, self.x + 32, self.y + 32)
+
+    def goto_dungeon1(self,world_object, player):
+        # 원본 world_object 에 삭제될 클래스만 빼고 복사
+        world_object[:] = [
+            obj for obj in world_object
+            if not isinstance(obj, (Town, NPC, Dummy, Portal))
+        ]
+        player.x, player.y = WIDTH // 2, 100
