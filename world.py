6df9ea3f3345a5a7e41d7605940660f97cab7c96
lovewinsall22 <taehyun0222@naver.com>
2025-10-18 00:10:35 +0900

    기존 worldObject 배열을 World 클래스로 만들어 레이어별로 보관

---
+++ world.py
@@ -0,0 +1,40 @@
+from pico2d import clear_canvas, update_canvas
+
+
+class World:
+    def __init__(self):
+        self.layers = {
+            'background' : [],
+            'object' : [],
+            'ui' : []
+        }
+
+    def add(self, obj, layer='object'):
+        self.layers[layer].append(obj)
+
+    def remove(self, obj):
+        for layer in self.layers.values():
+            if obj in layer:
+                layer.remove(obj)
+                return True
+        return False
+
+    def clear(self):
+        for layer in self.layers.values():
+            layer.clear()
+
+    def update(self):
+        for layer in self.layers.values():
+            for obj in layer:
+                if hasattr(obj, 'update'):
+                    obj.update()
+
+    def draw(self):
+        clear_canvas()
+        for bg in self.layers['background']:
+            bg.draw()
+        for e in self.layers['object']:
+            e.draw()
+        for ui in self.layers['ui']:
+            ui.draw()
+        update_canvas()
