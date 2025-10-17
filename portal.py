5fd4561bca32213ccaba323a90b48ca7aeb617c8
lovewinsall22 <taehyun0222@naver.com>
2025-10-17 22:35:39 +0900

    스테이지 별 맞는 포탈 출력

--- portal.py
+++ portal.py
@@ -7,17 +7,20 @@ from dungeon import Dungeon
 WIDTH, HEIGHT = 1280, 720
 
 class Portal:
-    def __init__(self,number,x,y):
+    def __init__(self,stage,number,x,y,dungeon):
         self.image = load_image('resource/portal.png')
+        self.stage = stage
         self.number = number
-        self.x , self.y = x, y
+        self.x, self.y = x, y
+        self.dungeon = dungeon
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(self.x,self.y,64,64)
-        draw_rectangle(self.x - 32, self.y - 32, self.x + 32, self.y + 32)
+        if self.dungeon.cur_dungeon == self.stage:
+            self.image.draw(self.x,self.y,64,64)
+            draw_rectangle(self.x - 32, self.y - 32, self.x + 32, self.y + 32)
 
     def goto_dungeon1(self,world_object, player, dungeon):
         # 원본 world_object 에 삭제될 클래스만 빼고 복사
