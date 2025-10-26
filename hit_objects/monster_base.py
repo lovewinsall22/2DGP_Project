09b6c2d0a33bb4a3c68646de6cad5162dbb10cb1
lovewinsall22 <taehyun0222@naver.com>
2025-10-26 19:15:12 +0900

    몬스터 공격충돌시 깜빡이며 피격애니메이션

--- hit_objects/monster_base.py
+++ hit_objects/monster_base.py
@@ -11,6 +11,11 @@ class Monster:
         self.alive = True
         self.player = player
 
+        self.hit_timer = 0  # 피격 지속 시간 (프레임 단위)
+        self.is_hit = False  # 현재 피격 상태 여부
+        self.flash_cycle = 0  # 깜빡임용 카운터
+
+
     def draw(self):
         pass
 
@@ -18,13 +23,18 @@ class Monster:
         pass
 
     def hitted(self, damage):
+        if self.alive:
+            self.is_hit = True
+            self.hit_timer = 30  # 약 0.6초간 피격 효과 유지 (30프레임 기준)
         self.hp -= damage
+        if self.hp <= 0:
+            self.alive = False
 
 
 class Golem(Monster):
     image = None
     def __init__(self, player = None):
-        super().__init__(randint(0, WIDTH), randint(0,HEIGHT), 100, 2,player)
+        super().__init__(randint(0, WIDTH), randint(0,HEIGHT), 10000, 2,player)
         if Golem.image == None:
             Golem.image = load_image('resource/l_golem.png')
         self.frame = randint(0,6)
@@ -36,6 +46,10 @@ class Golem(Monster):
 
     def draw(self):
         if self.alive == True:
+            if self.is_hit:
+                self.flash_cycle += 1
+                if (self.flash_cycle // 3) % 2 == 0:
+                    return  # 3프레임마다 안 보이게
             Golem.image.clip_draw(self.frame * 35, 0, 35, 35, self.x, self.y)
 
     def update(self):
@@ -43,6 +57,12 @@ class Golem(Monster):
         if self.alive == True and self.anicount % 10 == 0:
             self.frame = (self.frame + 1) % 7
             self.anicount = 0
+
+        if self.is_hit:
+            self.hit_timer -= 1
+            if self.hit_timer <= 0:
+                self.is_hit = False
+                self.flash_cycle = 0
 
         dx = self.player.x - self.x
         dy = self.player.y - self.y
