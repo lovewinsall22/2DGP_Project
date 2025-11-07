b486029656470eab8a960414f0c1739944734093
lovewinsall22 <taehyun0222@naver.com>
2025-11-07 16:48:46 +0900

    #프레임타임 구하는 로직 버그 수정

--- game_framework.py
+++ game_framework.py
@@ -47,15 +47,15 @@ def run(start_mode):
     start_mode.init()
 
     global frame_time
-    frame_time = 0
+    frame_time = 0.0
     current_time = time.time() # 현재시각 1970.1.1 이후
     while running:
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
         frame_time = time.time() - current_time # 로직-렌더링 시간
-        #frame_rate = 1.0 / frame_time # fps 구하기 초당 화면수
-        #current_time += frame_time
+        frame_rate = 1.0 / frame_time # fps 구하기 초당 화면수
+        current_time += frame_time
         #print(f'Frame Time: {frame_time}, Frame_rate: {frame_rate}')
 
     # repeatedly delete the top of the stack
