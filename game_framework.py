447fd55b2575e22eb00c0dc567c36de55231580c
lovewinsall22 <taehyun0222@naver.com>
2025-11-07 16:03:58 +0900

    #프레임타임 구하는 로직 작성 !!

--- game_framework.py
+++ game_framework.py
@@ -1,6 +1,7 @@
 running = None
 stack = None
-
+import time
+frame_time = 0.0
 
 def change_mode(mode):
     global stack
@@ -45,10 +46,17 @@ def run(start_mode):
     stack = [start_mode]
     start_mode.init()
 
+    global frame_time
+    frame_time = 0
+    current_time = time.time() # 현재시각 1970.1.1 이후
     while running:
         stack[-1].handle_events()
         stack[-1].update()
         stack[-1].draw()
+        frame_time = time.time() - current_time # 로직-렌더링 시간
+        #frame_rate = 1.0 / frame_time # fps 구하기 초당 화면수
+        #current_time += frame_time
+        #print(f'Frame Time: {frame_time}, Frame_rate: {frame_rate}')
 
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
