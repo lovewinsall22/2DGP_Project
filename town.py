3c6c4bf43781db83fb7c576f35b708d9ec176feb
lovewinsall22 <taehyun0222@naver.com>
2025-11-15 00:21:44 +0900

    마을 세부벽들 자료구조구성하여 그리기

--- town.py
+++ town.py
@@ -1,5 +1,18 @@
-from pico2d import load_image
+from pico2d import load_image, draw_rectangle
 WIDTH, HEIGHT = 1280, 720
+
+block = [
+    (50, 560, 240, 710),  # 첫번째 집
+    (290, 470, 460, 680),  # 두번째 집
+    (560, 400, 590, 720),  # 일자 벽
+    (725, 370, 800, 720),  # 위쪽 4개 연속벽
+    (800, 410, 850, 720),  # 위쪽 4개 연속벽
+    (850, 450, 900, 720),  # 위쪽 4개 연속벽
+    (900, 510, 1100, 720),  # 위쪽 4개 연속벽
+    (830, 250, 900, 300),  # 아래쪽 3개 연속벽
+    (900, 250, 960, 370),  # 아래쪽 3개 연속벽
+    (960, 250, 1280, 420)  # 아래쪽 3개 연속벽
+]
 
 class Town:
     def __init__(self):
@@ -7,6 +20,8 @@ class Town:
 
     def draw(self):
         self.image.draw(WIDTH // 2, HEIGHT // 2, WIDTH, HEIGHT)
+        for l, b, r, t in block:
+            draw_rectangle(l, b, r, t)
 
     def update(self):
         pass
