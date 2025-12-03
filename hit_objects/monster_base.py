53dcac761d06e145f824bb49e00c0b600ed2ba51
lovewinsall22 <taehyun0222@naver.com>
2025-12-03 21:58:12 +0900

    아이스 골렘 클래스 추가

--- hit_objects/monster_base.py
+++ hit_objects/monster_base.py
@@ -221,6 +221,99 @@ class Boss(Monster):
         elif group == 'player:boss':
             pass
 
+class Ice_Golem(Monster):
+    image = None
+    def __init__(self, player = None):
+        super().__init__(randint(0, WIDTH), randint(0,HEIGHT), 3000, 2,player)
+        if Ice_Golem.image == None:
+            Ice_Golem.image = load_image('resource/ice_golem.png')
+        self.frame = randint(0,6)
+        self.speed = 0.7
+        self.on_right = randint(0,1) # 캐릭터기준 오른쪽인지 ,,
+        self.attack_range = 50
+
+
+    def draw(self):
+        if not self.alive:
+            return
+
+        if self.is_hit and (self.flash_timer // 5) % 2 == 0:
+            return  # 5프레임마다 안 그려짐
+
+        if self.spawn_effect:
+            if self.scale < self.scale_target:
+                self.scale += self.scale_speed
+            else:
+                self.scale = self.scale_target
+                self.spawn_effect = False  # 등장 완료
+
+        size = int(70 * self.scale)
+
+        if self.on_right:
+            Ice_Golem.image.clip_composite_draw(int(self.frame) * 42, 0, 35, 35, 0, 'h', self.x, self.y, size, size)
+        else:
+            Ice_Golem.image.clip_composite_draw(int(self.frame) * 42, 0, 35, 35, 0, '', self.x, self.y, size, size)
+        draw_rectangle(*self.get_bb())
+        self.font.draw(self.x, self.y + 15, f'(hp: {self.hp})', (255, 0, 0))
+
+    def update(self):
+        if not self.alive:
+            self.respawn_timer += 1
+            if self.respawn_timer >= 300:
+                self.alive = True
+                self.respawn_timer = 0
+            return
+
+        if self.is_hit:
+            self.stop_time -= 1
+            self.flash_timer += 1
+            if self.stop_time <= 0:
+                self.is_hit = False
+                self.stop_time = 120
+                self.flash_timer = 0
+                if self.hp <= 0:
+                    self.alive = False
+                    game_world.remove(self)
+                    self.player.get_money_animation = True
+                    self.player.gold += randint(10,50)
+
+                    # 골렘 리스폰
+                    respawn_golem = Red_Golem(self.player)
+                    game_world.add(respawn_golem, 'object')
+                    game_world.add_collision_pair('player:golem', self.player, respawn_golem)
+                    game_world.add_collision_pair('sword:golem', self.player.sword, respawn_golem)
+                    print(f'[DEBUG] Red Golem Respawned')
+            return
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 7
+
+        if self.trace_on:
+            dx = self.player.x - self.x
+            dy = self.player.y - self.y
+            distance = sqrt(dx * dx + dy * dy)
+            if dx >= 0:
+                self.on_right = True
+            else:
+                self.on_right = False
+            if distance > self.attack_range:
+                self.x += self.speed * (dx / distance)
+                self.y += self.speed * (dy / distance)
+        else:
+            self.trace_wait -= 1
+            if self.trace_wait <= 0:
+                self.trace_on = True
+
+    def get_bb(self):
+        return self.x - 16*2, self.y - 16*2, self.x + 16*2, self.y + 16*2
+
+    def handle_collision(self, group, other):
+        if group == 'sword:golem':
+            if other.sword_active and not self.is_hit:
+                self.hp -= other.damage
+                self.is_hit = True
+                self.flash_timer = 0
+                self.trace_on = True
+        elif group == 'player:golem':
+            pass
 
 class Red_Golem(Monster):
     image = None
