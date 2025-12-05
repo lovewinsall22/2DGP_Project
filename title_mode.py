06148dbafcc05df49c8750a0f31ff747fd7b75e1
lovewinsall22 <taehyun0222@naver.com>
2025-12-06 01:41:32 +0900

    타이틀모드에서 타이틀bgm 실행후 엔터눌러 플레이모드 실행될때 플레이bgm실행

--- title_mode.py
+++ title_mode.py
@@ -3,12 +3,15 @@ WIDTH, HEIGHT = 1280, 720
 import game_framework, play_mode
 
 image = None
-
+title_music = None
+play_music = None
 def init():
     # 타이틀 이미지 로드
-    global image
+    global image, title_music
     image = load_image('resource/title.png')
-
+    title_music = load_music('resource/bgm/title.wav')
+    title_music.set_volume(64)
+    title_music.repeat_play()
 
 def update():
     pass
@@ -38,3 +41,9 @@ def handle_events():
                 game_framework.quit()
             elif event.key == SDLK_RETURN:
                 game_framework.change_mode(play_mode)
+                global title_music, play_music
+                title_music.stop()
+                play_music = load_music('resource/bgm/play.wav')
+                play_music.set_volume(64)
+                play_music.repeat_play()
+
